62d1ba9a33e8f9b61ea8865f0c3f13fe6f2caa8d
kvartoom <kvartoom@gmail.com>
2023-07-29 15:28:34 +0300

    6. Правила Checkstyle  [#504981 #432801]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -4,25 +4,25 @@ public class Broken {
 
     private int sizeOfEmpty = 10;
 
-    public String surname;
+    private String surname;
 
-    public static final String NEWVALUE = "";
+    public static final String NEW_VALUE = "";
 
-    void echo() {
+    public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
         int h;
     }
 
-    String name;
+    private String name;
 
-    Broken() {
+    public Broken() {
     }
 }
