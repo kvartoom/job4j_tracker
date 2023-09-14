f3e6c1f2e59f48d40bfd5883b7fa11fcfd05be78
kvartoom <kvartoom@gmail.com>
2023-09-14 23:22:41 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getDateTime() {
+        return created;
+    }
 
     public Item() {
     }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime dateTime = item.getDateTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTime = dateTime.format(formatter);
+        System.out.println(currentDateTime);
+    }
+}
