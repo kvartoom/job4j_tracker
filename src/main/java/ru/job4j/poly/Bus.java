e523631decb3eef0c090a2b57e01c40ef20b5e53
kvartoom <kvartoom@gmail.com>
2023-10-15 17:46:42 +0300

    4.2. Интерфейс Input [#181092 #451622]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,27 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Чтобы начать движение нажмите педаль газа");
     }
 
     @Override
     public void passenger(int number) {
-
+        if (number <= 1) {
+            System.out.println("Дождаться пассажиров");
+        } else {
+            System.out.println("Начать движение");
+        }
     }
 
     @Override
     public int fill(int fuel) {
-        return 0;
+        int price = 0;
+        if (fuel == 0) {
+            System.out.println("Кончилось топливо, заправьте машину");
+            price = 1500;
+        } else {
+            System.out.println("Остаток топлива" + fuel);
+        }
+        return price;
     }
 }
